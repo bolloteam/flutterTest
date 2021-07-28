9578c92d2329f33c7fbdeff4797e59b2de669de4
bolloteam <manelito@gmail.com>
2021-07-28 08:32:08 -0700

    Scaffold and import new Page

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,12 @@
-import 'dart:html';
-
 import 'package:flutter/material.dart';
-
+import 'pages/home_page.dart';
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget{
   @override
   Widget build(BuildContext context){
     return MaterialApp(
-      home: Text("Meñu"),
+      home: HomePage(),
     );
   }
 }
