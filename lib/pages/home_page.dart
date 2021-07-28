3883955692d2613a46505e1ca65755dace651536
bolloteam <manelito@gmail.com>
2021-07-28 11:12:15 -0700

    Widget Icon and SVG package

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,5 +1,7 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import '../widgets/circle_container.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 
 class HomePage extends StatelessWidget{
   @override
@@ -11,10 +13,24 @@ class HomePage extends StatelessWidget{
       body: Center(
         child: 
           CircleContainer(
-            child: Text(
+            child: 
+            SvgPicture.asset(
+              'assets/icons/sun.svg', 
+              width: 40, 
+              height: 40, 
+            ),
+            /*
+            Icon(
+              //CupertinoIcons.add,
+              Icons.phone,
+
+              size: 50,
+              color: Colors.blue
+              ),*/
+            /*Text(
               "sape",
               style: TextStyle(fontSize: 40, color: Colors.amber),
-            ),
+            ),*/
             width:100,
             height: 100,
         ),
