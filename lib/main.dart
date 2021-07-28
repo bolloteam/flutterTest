fe508f816da969d5c49a2fb07a7ff8675daa358c
bolloteam <manelito@gmail.com>
2021-07-28 12:15:36 -0700

    Widget SafeArea

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,17 @@
 import 'package:flutter/material.dart';
 import 'pages/home_page.dart';
+import 'package:flutter/services.dart';
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget{
   @override
   Widget build(BuildContext context){
+
+    SystemChrome.setSystemUIOverlayStyle(SystemUiOverlayStyle.light);
+
     return MaterialApp(
       home: HomePage(),
+      debugShowCheckedModeBanner: false,
     );
   }
 }
--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -7,32 +7,14 @@ class HomePage extends StatelessWidget{
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text("My App"),
-      ),
-      body: Center(
-        child: 
-          CircleContainer(
-            child: 
-            SvgPicture.asset(
-              'assets/icons/sun.svg', 
-              width: 40, 
-              height: 40, 
-            ),
-            /*
-            Icon(
-              //CupertinoIcons.add,
-              Icons.phone,
-
-              size: 50,
-              color: Colors.blue
-              ),*/
-            /*Text(
-              "sape",
-              style: TextStyle(fontSize: 40, color: Colors.amber),
-            ),*/
-            width:100,
-            height: 100,
+      backgroundColor: Colors.white,
+      body: SafeArea(
+        top: true,
+        bottom: true,
+        child: Container(
+          child: 
+          Text("HOLA papu"),
+          //alignment: Alignment.bottomCenter,
         ),
       ),
     );
